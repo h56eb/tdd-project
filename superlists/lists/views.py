14395172a781fb18e1274877dcdb480d62bad625
h56eb <h3ct0r-leoni@hotmail.com>
2026-09-01 01:20:30 -0300

    Ciclo: Teste de Unidade/Desenvolvimento

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
-def home_page():
+def home_page(request):
+    return HttpResponse()
+
+def home_page(request):
     pass
+
+# Create your views here.
+def home_page(request):
+    return HttpResponse('<html>')
+
+# Create your views here.
+def home_page(request):
+    return HttpResponse('<html><title>To-Do lists</title>')
+
+# Create your views here.
+def home_page(request):
+    return HttpResponse('<html><title>To-Do lists</title></html>')
+
