fde6cb97a8d758941b48737d1b5e44c98fecb2a9
h56eb <h3ct0r-leoni@hotmail.com>
2026-09-09 08:48:22 -0300

    Processando Requisição POST

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -2,3 +2,11 @@ from django.shortcuts import render
 
 def home_page(request):
     return render(request, 'home.html')
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home_page(request):
+    if request.method == 'POST':
+        return HttpResponse(request.POST['item_text'])
+    return render(request, 'home.html')
