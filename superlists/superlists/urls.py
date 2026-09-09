cb56f8360df2420111b23f605a60a758bca17f2e
h56eb <h3ct0r-leoni@hotmail.com>
2026-09-09 08:55:34 -0300

    Regressão

--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -1,6 +1,6 @@
-from django.conf.urls import url
+from django.urls import path
 from lists import views
 
 urlpatterns = [
-    url(r'^$', views.home_page, name='home'),
+    path('', views.home_page, name='home'),
 ]
